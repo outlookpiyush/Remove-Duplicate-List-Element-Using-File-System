0b160f413f618f6cb351ef8d1ac6a571541bbaf9
Maersk Test <goriya.piyush@gmail.com>
2020-05-29 16:57:54 +0530

    Add project files.

---
+++ CapitaExam/LoadListFromTextFileAndRemoveDuplicateElementFromStringList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CapitaExam
+{
+    public class LoadListFromTextFileAndRemoveDuplicateElementFromStringList
+    {
+        /// <summary>
+        /// Remove Duplicate Element From String List
+        /// </summary>
+        public void RemoveDuplicateElement()
+        {
+            List<string> features = new List<string>(File.ReadAllLines(@"Files\Features.txt"));
+            List<string> featureToDelete = new List<string>(File.ReadAllLines(@"Files\FeaturesToDelete.txt"));
+            foreach (var deletestring in featureToDelete)
+            {
+                var listIndex = features.FindIndex(c => c.Contains(deletestring));
+                features.RemoveAt(listIndex);
+            }
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "ExpectedOutPut.txt")))
+            {
+                foreach (var line in features)
+                    outputFile.WriteLine(line);
+
+                outputFile.Dispose();
+                outputFile.Close();
+            }
+        }
+    }
+}
---
+++ CapitaExam/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CapitaExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objList = new LoadListFromTextFileAndRemoveDuplicateElementFromStringList();
+            objList.RemoveDuplicateElement();
+        }
+        
+    }
+}
